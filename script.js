8793b12d5f6deab5fb73f51b520465d68be3d27d
SabaTsaava017 <sabacaava7@gmail.com>
2023-05-26 00:40:25 +0400

    creat script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+for(let i = 0; i < 100; i++ ){
+    console.log(i)
+}
+
+let WhileNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+let index = 0;
+while(index < WhileNumber.length);{
+    console.log(WhileNumber[index])
+    index++;
+}
+
+let Names = ["Saba", "Lizi", "Mariami", "Nika", "Gio"]
+Names.unshift("Tika");
+Names.push("Tekla");
+Names.shift("Sandro");
+Names.pop("Lika");
+
+console.log(Names);
+
+
